858c751ad431b6a885575e27847240e95f5ecdf2
samueljustus <samie.justus@gmail.com>
2023-11-14 16:56:40 +0100

    new functionality coming soon

--- app.js
+++ app.js
@@ -29,49 +29,80 @@ form.addEventListener('submit', (e) => {
     localStorage.setItem('task', JSON.stringify(todoArray))
 
     createTask(task)
-    countArray()
     form.reset()
     input.focus()
+    countItemsLeft()
+
+    // countArray()
 }) 
 
-function createTask(task) {
-    const taskEl = document.createElement('li')
-    taskEl.classList.add('todo-item')
-    // taskEl.setAttribute('id', task.id)
-
-    const checkImg = document.createElement('img')
-    checkImg.classList.add('check')
-    checkImg.src = 'img/icon-check.svg'
-    
-    const radio = document.createElement('span')
-    radio.classList.add('radio')
-
-    const todoText = document.createElement('p')
-    todoText.classList.add('todo-text')
-    todoText.innerText = task.name
-
-    const crossImg = document.createElement('img')
-    crossImg.classList.add('cross')
-    crossImg.src = 'img/icon-cross.svg'
 
 
-    
-    if (task.isCompleted) {
-        taskEl.classList.add('complete')
-        radio.classList.add('radio_checked')
-        todoText.classList.add('strike-through')
+    function removeTask(taskId) {
+        const newTodoArray = todoArray.filter(() => {
+            task.id !== taskId
+        })
+        localStorage.setItem('task', JSON.stringify(newTodoArray))
+        document.getElementById(taskId).remove()
+        countItemsLeft()
     }
+    
+    function createTask(task) {
+        const taskEl = document.createElement('li')
+        taskEl.classList.add('todo-item')
+        taskEl.setAttribute('id', task.id)
+        
+        const checkImg = document.createElement('img')
+        checkImg.classList.add('check')
+        checkImg.src = 'img/icon-check.svg'
+        
+        const radio = document.createElement('span')
+        radio.classList.add('radio')
+        
+        const todoText = document.createElement('p')
+        todoText.classList.add('todo-text')
+        todoText.innerText = task.name
+        
+        const crossImg = document.createElement('img')
+        crossImg.classList.add('cross')
+        crossImg.src = 'img/icon-cross.svg'
+        
 
+        
+        crossImg.addEventListener('click', (e) => {
+            // if (e.target.classList.contains('cross')) {
+            //     const taskId = e.target.closest('li').id
+            //     removeTask(taskId)
+            // }
+        
+            console.log('clicked')
+        })
+        
+        if (task.isCompleted) {
+            taskEl.classList.add('complete')
+            radio.classList.add('radio_checked')
+            todoText.classList.add('strike-through')
+    }
+    
     taskEl.append(checkImg, radio, todoText, crossImg)
     todoContainer.append(taskEl)
- }
-
-function countArray() {
-    const filteredArray = todoArray.filter((task) => {
-        (task.isCompleted)
-    })
-
-    console.log(filteredArray.length)
 }
+
+function countItemsLeft() {
+    const itemsLeftAarray = todoArray.filter((task) => {
+        task.isCompleted === false;
+    })
+    
+    itemsLeft.textContent = todoArray.length  
+    localStorage.setItem('task', JSON.stringify(todoArray))
+    
+}
+
+
+
+
+// ABANDONED FOR NOW
+
+// MI O FI E LE O  
 
 
